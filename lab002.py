ce43ed49bd8aa8f36b2d030e4cd8b0df28b84557
PimpaleJyoti <147304311+PimpaleJyoti@users.noreply.github.com>
2023-10-08 15:00:18 +0530

    Update lab002.py

--- lab002.py
+++ lab002.py
@@ -1,9 +1,12 @@
+#Print the Name of The User
 name=input("Name of The User")
 print(name)
 
+#Take 1st Name & Last Name of user & print the Sep="_" & end with\t
 fname=(input("First Name"))
 lname=(input("Last Name"))
 print(fname,lname,sep='_',end='\t')
+#Take User input name & say with message You are welcome"jyoti" to the python class
 
 name=input('Enter your Name')
 print("you are welcome",name)
