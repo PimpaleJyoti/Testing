e43f5655e11489622a1fc5a3c5494f6d3c4f8a59
PimpaleJyoti <147304311+PimpaleJyoti@users.noreply.github.com>
2023-10-08 15:03:04 +0530

    Update Lab001.py

--- Lab001.py
+++ Lab001.py
@@ -3,3 +3,22 @@ print("Hello Jyoti")
 print(23+45)
 print(23*45)
 print(10/2)
+
+
+
+2.Compiler & Interpretation difference
+
+A Compiler takes a program as a whole.
+
+The Compilers generate intermediate machine codes
+
+Examples of compiled programming languages are C and C++. 
+
+
+An Interpreter takes single lines of a code.
+
+The Interpreters never generate any intermediate machine codes.
+
+Interpreter directly executes instructions written in a programming or scripting language without previously converting them to an object code or machine code.
+
+Examples of interpreted languages are Perl, Python and MATLAB .
